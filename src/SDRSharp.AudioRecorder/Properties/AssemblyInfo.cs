496a01879cd32b5ca2fcbe5f590c8b069dee666b
Merlin <20001060+savadom13@users.noreply.github.com>
2026-06-21 23:35:26 +0300

    gha6

--- src/SDRSharp.AudioRecorder/Properties/AssemblyInfo.cs
+++ src/SDRSharp.AudioRecorder/Properties/AssemblyInfo.cs
@@ -1,19 +1,3 @@
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
-using System.Security;
-using System.Security.Permissions;
-
-[assembly: AssemblyTitle("Audio Recorder Plug-in for SDR#")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Updated by thewraith2008")]
-[assembly: AssemblyProduct("Modified by thewraith2008 (Original by Ian Gilmour and TSSDR)")]
-[assembly: AssemblyCopyright("Modified by thewraith2008 (Original by Ian Gilmour and TSSDR)")]
-[assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("20600a3f-d333-446a-bb1a-a74de8122045")]
-[assembly: AssemblyFileVersion("1.3.10.0")]
-[assembly: AssemblyVersion("1.3.10.0")]
